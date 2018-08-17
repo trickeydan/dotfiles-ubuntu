05d8691478e7eb616d19bc2f8e6874561ec59e1f
Dan Trickey <dan@trickey.io>
2018-08-17 18:46:31 +0100

    Add LightDM Background

--- laptop.sh
+++ laptop.sh
@@ -2,7 +2,7 @@ sudo add-apt-repository ppa:ansible/ansible
 sudo apt update
 sudo apt install ansible git -y
 cd ~
-git clone https://github.com/trickeydan/dotfiles.git
-cd dotfiles
+git clone https://github.com/trickeydan/dotfiles.git .dotfiles
+cd .dotfiles
 sudo ansible-playbook -i hosts laptop.yml
 sudo reboot
