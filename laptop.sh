832e7df78ddcf3bbe226e89ff8f4c6f9c2bf0e4a
Dan Trickey <dan@trickey.io>
2018-08-17 14:24:06 +0100

    Auto laptop script

---
+++ laptop.sh
@@ -0,0 +1,7 @@
+sudo add-apt-repository ppa:ansible/ansible
+sudo apt update
+sudo apt install ansible git -y
+cd ~
+git clone https://github.com/trickeydan/dotfiles.git
+cd dotfiles
+sudo ansible-playbook -i hosts laptop.yml
