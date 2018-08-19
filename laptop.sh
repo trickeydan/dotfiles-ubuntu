627a9ff5071e867e530dc3fddd4440f1fe373e6b
Dan Trickey <dan@trickey.io>
2018-08-19 14:24:55 +0100

    Setup password manager

--- laptop.sh
+++ laptop.sh
@@ -4,5 +4,5 @@ sudo apt install ansible git -y
 cd ~
 git clone https://github.com/trickeydan/dotfiles.git .dotfiles
 cd .dotfiles
-sudo ansible-playbook -i hosts laptop.yml
+sudo ansible-playbook -i hosts laptop.yml --ask-vault-pass
 sudo reboot
